211621d45ad15631a5be6f781810428f6aa98c5c
hajra-javed <hajra_95@hotmail.com>
2023-03-13 19:05:31 +0500

    Added form functionality with validation

--- App.js
+++ App.js
@@ -1,1 +1,47 @@
-const [firstName, lastName, email, phoneNumber, password, confirmPassword] = document.querySelectorAll('input');
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
+const btn = document.querySelector('button');
+
+
+btn.addEventListener('click', (e) => {
+    // e.preventDefault();
+    for (let i = 0; i < inputs.length - 1; i++) {
+        if (inputs[i].required && inputs[i].value === '') {
+            // console.log(inputs[i].type);
+            inputs[i].classList.add('error');
+            return;
+        }
+    }
+    if (password.value !== confirmPassword.value){
+        confirmPassword.classList.add('error');
+        confirmPassword.setCustomValidity('Passwords do not match');
+    } else {
+        confirmPassword.setCustomValidity('');
+    }
+});
+
+confirmPassword.addEventListener('keydown', () => {
+    confirmPassword.classList.remove('error');
+    confirmPassword.setCustomValidity('');
+})
+
+// inputs.forEach((i) => {
+//     i.addEventListener('keydown', () => {
+//         console.log(i.value);
+//         i.classList.remove('error');
+
+//     })
+// });
+
+// confirmPassword.addEventListener('keyup', () => {
+//     if (password.value !== confirmPassword.value) {
+//         confirmPassword.classList.add('error');
+//         // confirmPassword.setCustomValidity('Passwords do not match');
+//     }
+//     else {
+//         p.classList.remove('error');
+//     }
+// });
+
